93ce7f98606eb5060af844ebc42e802f8b786732
dengdeying <dengdeying1980@gmail.com>
2016-09-05 16:59:05 +0800

    init commit

---
+++ application/src/main/java/com/airchina/demo/application/HelloController.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.airchina.demo.application;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author dengdeying
+ *
+ */
+@RestController
+public class HelloController {
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+}
