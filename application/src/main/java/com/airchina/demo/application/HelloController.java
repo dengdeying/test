39d30a73d88e067806d6c0de83a0992d516f78e9
dengdeying <dengdeying1980@gmail.com>
2016-09-05 17:08:39 +0800

    3rd

--- application/src/main/java/com/airchina/demo/application/HelloController.java
+++ application/src/main/java/com/airchina/demo/application/HelloController.java
@@ -14,6 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!!!";
+		return "Greetings from Spring Boot!!!!";
 	}
 }
