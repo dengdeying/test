29ec1d94739af5b94d114b1763c0b7c8ac6b1c59
dengdeying <dengdeying1980@gmail.com>
2016-09-06 13:13:51 +0800

    commit master

--- application/src/main/java/com/airchina/demo/application/HelloController.java
+++ application/src/main/java/com/airchina/demo/application/HelloController.java
@@ -14,6 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@RequestMapping("/")
 	public String index() {
-		return "my branch 3";
+		return "master";
 	}
 }
